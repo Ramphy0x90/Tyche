9d06380fd8eee34b2dc8dac1447ff5eb4a926bb8
ramphy0x90 <ramphy11@hotmail.com>
2023-12-02 16:09:27 +0100

    min: update

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
+import { TransactionsComponent } from './containers/transactions/transactions.component';
+import { ReportsComponent } from './containers/reports/reports.component';
+import { ChartOfAccountsComponent } from './containers/chart-of-accounts/chart-of-accounts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "login" },
+    { path: "home", component: HomeComponent },
+    { path: "transactions", component: TransactionsComponent },
+    { path: "reports", component: ReportsComponent },
+    { path: "chart-of-accounts", component: ChartOfAccountsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavBarComponent } from './components/side-nav-bar/side-nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        SideNavBarComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
